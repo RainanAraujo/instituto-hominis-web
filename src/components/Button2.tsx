e2cd79bada1cb00a28d8f5ad7888633f281c05cc
Rainan Carneiro Araújo <rainanaraaujo@gmail.com>
2023-02-02 15:17:15 -0300

    merge files

--- src/components/Button2.tsx
+++
@@ -1,9 +0,0 @@
-import { Button2 } from "../styles/components/Button2";
-
-export default function Posts() {
-  return (
-    <Button2>
-      <a href="#">Ver todos os posts</a>
-    </Button2>
-  );
-}
